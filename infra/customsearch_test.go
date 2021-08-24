f3c1a9b07d285618fc6906aac2a4c07c5a278ab9
johnmanjiro13 <juntama193@gmail.com>
2021-08-24 20:09:53 +0900

    draw string test

--- infra/customsearch_test.go
+++ infra/customsearch_test.go
@@ -3,7 +3,9 @@ package infra
 import (
 	"bytes"
 	"image"
+	"image/jpeg"
 	"image/png"
+	"io"
 	"os"
 	"testing"
 
@@ -31,4 +33,25 @@ func TestToPng(t *testing.T) {
 	img, format, err = image.Decode(buf)
 	assert.NoError(t, err)
 	assert.Equal(t, "png", format)
+}
+
+func TestDrawStringToImage(t *testing.T) {
+	file, err := os.Open("testdata/lgtm.jpg")
+	assert.NoError(t, err)
+	expected, err := io.ReadAll(file)
+	assert.NoError(t, err)
+
+	file, err = os.Open("testdata/image.jpg")
+	assert.NoError(t, err)
+	img, _, err := image.Decode(file)
+	assert.NoError(t, err)
+
+	res, err := drawStringToImage(img, "LGTM")
+	assert.NoError(t, err)
+
+	actual := new(bytes.Buffer)
+	err = jpeg.Encode(actual, res, &jpeg.Options{Quality: 100})
+	assert.NoError(t, err)
+
+	assert.Equal(t, expected, actual.Bytes())
 }
